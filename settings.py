eeb621c368349d0b18fb096bbc07324eefe0a0c0
Romariodev <arg.dev.java@gmail.com>
2026-07-08 12:14:44 -0300

    Fix: simplify DATABASES configuration in settings.py for tests

--- settings.py
+++ settings.py
@@ -1,3 +1,24 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
+    }
+}
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Europe/Kiev"
+
+USE_I18N = True
+
 USE_TZ = False
 
 INSTALLED_APPS = [
